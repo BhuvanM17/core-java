d20ff43b9ef61a5a5d04e32f382b7d7b82401ad5
BhuvanM17 <bhuvan.xworkz@gmail.com>
2024-07-18 22:02:37 +0530

    instance variables usage

---
+++ objects/Garden.java
@@ -0,0 +1,15 @@
+class Garden {
+    String name;
+    String place;
+
+    public void createGarden(String name, String place) {
+        this.name = name;
+        this.place = place;
+    }
+
+    public void displayGardenDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Place: " + place);
+        System.out.println();
+    }
+}
---
+++ objects/GardenRunner.java
@@ -0,0 +1,63 @@
+public class GardenRunner {
+    public static void main(String[] plants) {
+        Garden garden1 = new Garden();
+        garden1.createGarden("Grapes Garden", "Jalahalli");
+        garden1.displayGardenDetails();
+
+        Garden garden2 = new Garden();
+        garden2.createGarden("Botanical Garden", "Chandragiri");
+        garden2.displayGardenDetails();
+
+        Garden garden3 = new Garden();
+        garden3.createGarden("Colour Garden", "Mumbai");
+        garden3.displayGardenDetails();
+
+        Garden garden4 = new Garden();
+        garden4.createGarden("Bog Garden", "Carolina");
+        garden4.displayGardenDetails();
+
+        Garden garden5 = new Garden();
+        garden5.createGarden("Cottage Garden", "England");
+        garden5.displayGardenDetails();
+
+        Garden garden6 = new Garden();
+        garden6.createGarden("Enchanted Garden", "Delhi");
+        garden6.displayGardenDetails();
+
+        Garden garden7 = new Garden();
+        garden7.createGarden("Evergreen Garden", "Kenya");
+        garden7.displayGardenDetails();
+
+        Garden garden8 = new Garden();
+        garden8.createGarden("Garden of Dreams", "Europe");
+        garden8.displayGardenDetails();
+
+        Garden garden9 = new Garden();
+        garden9.createGarden("Dry Garden", "Japan");
+        garden9.displayGardenDetails();
+
+        Garden garden10 = new Garden();
+        garden10.createGarden("Moonlit Garden", "Bangalore");
+        garden10.displayGardenDetails();
+
+        Garden garden11 = new Garden();
+        garden11.createGarden("Gardens by the Bay", "Singapore");
+        garden11.displayGardenDetails();
+
+        Garden garden12 = new Garden();
+        garden12.createGarden("Kew Garden", "Chennai");
+        garden12.displayGardenDetails();
+
+        Garden garden13 = new Garden();
+        garden13.createGarden("Monet's Garden", "New York");
+        garden13.displayGardenDetails();
+
+        Garden garden14 = new Garden();
+        garden14.createGarden("Vegetable Garden", "Karnataka");
+        garden14.displayGardenDetails();
+
+        Garden garden15 = new Garden();
+        garden15.createGarden("Monikers", "Delhi");
+        garden15.displayGardenDetails();
+    }
+}
